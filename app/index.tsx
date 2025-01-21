70e85e3dd2d42cc01321f3b845880a75f5cb6b9f
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2025-01-21 17:05:43 +0100

    add background Remove Stack Change Icon

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import { Image } from 'expo-image';
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
+  const blurhash =
+    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source="https://wallpaperaccess.com/full/1533684.jpg"
+        placeholder={{ blurhash }}
+        contentFit="cover"
+        transition={100}
+      />
     </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#0553',
+  },
+
+});
